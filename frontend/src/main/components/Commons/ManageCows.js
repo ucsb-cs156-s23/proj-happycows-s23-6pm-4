65a94f82c5be28b92e0c66789189b96583d72f80
Jacob Woodward <jacobwoodward@ucsb.edu>
2023-05-26 16:13:14 -0700

    jw-add buttons to buy and sell multiple cows

--- frontend/src/main/components/Commons/ManageCows.js
+++ frontend/src/main/components/Commons/ManageCows.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Card, Button, Row, Col } from "react-bootstrap";
 import cowHead from "./../../../assets/CowHead.png"; 
 
-// add parameters 
-const ManageCows = ({userCommons,commons, onBuy, onSell}) =>  {
+
+// add parameters
+const ManageCows = ({userCommons, commons, onBuy, onSell}) =>  {
     // update cowPrice from fixture
+    // Stryker disable ArithmeticOperator : line 24: not very important and unsure of how to test this
     return (
         <Card>
         <Card.Header as="h5">Manage Cows</Card.Header>
         <Card.Body>
-            {/* change $10 to info from fixture */}
+            {/* why does removing the optional chaining question mark turn the entire page white? */}
             <Card.Title>Market Cow Price: ${commons?.cowPrice}</Card.Title>
             <Card.Title>Number of Cows: {userCommons.numOfCows}</Card.Title>
                 <Row>
@@ -19,10 +21,22 @@ const ManageCows = ({userCommons,commons, onBuy, onSell}) =>  {
                         </Card.Text>
                     </Col>
                     <Col>
-                        <Button variant="outline-danger" onClick={()=>{onBuy(userCommons)}} data-testid={"buy-cow-button"}>Buy cow</Button>
+                        <Button variant="outline-danger" onClick={()=>{onBuy(userCommons, parseInt(userCommons.totalWealth/commons.cowPrice))}} data-testid={"buy-max-cows-button"}>Buy Max Cows</Button>
                         <br/>
                         <br/>
-                        <Button variant="outline-danger" onClick={()=>{onSell(userCommons)}} data-testid={"sell-cow-button"}>Sell cow</Button>
+                        <Button variant="outline-danger" onClick={()=>{onBuy(userCommons, 10)}} data-testid={"buy-10-cows-button"}>Buy 10 Cows</Button>
+                        <br/>
+                        <br/>
+                        <Button variant="outline-danger" onClick={()=>{onBuy(userCommons, 1)}} data-testid={"buy-a-cow-button"}>Buy a Cow</Button>
+                        <br/>
+                        <br/>
+                        <Button variant="outline-danger" onClick={()=>{onSell(userCommons, 1)}} data-testid={"sell-a-cow-button"}>Sell a Cow</Button>
+                        <br/>
+                        <br/>
+                        <Button variant="outline-danger" onClick={()=>{onSell(userCommons, 10)}} data-testid={"sell-10-cows-button"}>Sell 10 Cows</Button>
+                        <br/>
+                        <br/>
+                        <Button variant="outline-danger" onClick={()=>{onSell(userCommons, userCommons.numOfCows)}} data-testid={"sell-all-cows-button"}>Sell All Cows</Button>
                         <br/>
                         <br/>
                     </Col>
--- frontend/src/tests/components/Commons/ManageCows.test.js
+++ frontend/src/tests/components/Commons/ManageCows.test.js
@@ -1,30 +1,43 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import ManageCows from "main/components/Commons/ManageCows"; 
 import userCommonsFixtures from "fixtures/userCommonsFixtures"; 
+import commonsFixtures from "fixtures/commonsFixtures";
 
 describe("ManageCows tests", () => {
+
     test("renders without crashing", () => {
         render(
             <ManageCows userCommons = {userCommonsFixtures.oneUserCommons[0]} onBuy={(userCommons) => { console.log("onBuy called:",userCommons); }} onSell={ (userCommons) => { console.log("onSell called:",userCommons); }} />
         );
     });
 
-    test("buying and selling a cow", async () => {
+    test("buying and selling cows", async () => {
         const mockBuy = jest.fn();
         const mockSell = jest.fn();
 
         render(
-            <ManageCows userCommons = {userCommonsFixtures.oneUserCommons[0]} onBuy={mockBuy} onSell={mockSell} />
+            <ManageCows userCommons = {userCommonsFixtures.oneUserCommons[0]} commons = {commonsFixtures.oneCommons[0]} onBuy={mockBuy} onSell={mockSell} />
         );
 
-        const buyButton = screen.getByTestId("buy-cow-button");
-        const sellButton = screen.getByTestId("sell-cow-button");
+        const buyMaxButton = screen.getByTestId("buy-max-cows-button");
+        const buyTenButton = screen.getByTestId("buy-10-cows-button");
+        const buyOneButton = screen.getByTestId("buy-a-cow-button");
+        const sellOneButton = screen.getByTestId("sell-a-cow-button");
+        const sellTenButton = screen.getByTestId("sell-10-cows-button");
+        const sellAllButton = screen.getByTestId("sell-all-cows-button");
         
-        fireEvent.click(buyButton);
-        await waitFor( ()=>expect(mockBuy).toHaveBeenCalled() );
-
-        fireEvent.click(sellButton);
-        await waitFor( ()=>expect(mockSell).toHaveBeenCalled() );
+        fireEvent.click(buyMaxButton);
+        await waitFor( ()=>expect(mockBuy).toHaveBeenCalled());
+        fireEvent.click(buyTenButton);
+        await waitFor( ()=>expect(mockBuy).toHaveBeenCalled());
+        fireEvent.click(buyOneButton);
+        await waitFor( ()=>expect(mockBuy).toHaveBeenCalled());
+        fireEvent.click(sellOneButton);
+        await waitFor( ()=>expect(mockSell).toHaveBeenCalled());
+        fireEvent.click(sellTenButton);
+        await waitFor( ()=>expect(mockSell).toHaveBeenCalled());
+        fireEvent.click(sellAllButton);
+        await waitFor( ()=>expect(mockSell).toHaveBeenCalled());
         
     });
 
