b733594f1aa20bfa0af1fe3b8e47fcd49bcffa1b
Jessica Ho <shin-yi@umail.ucsb.edu>
2023-05-31 18:56:41 -0700

    Redesign the user commons page (Make key user statistics clear)

--- frontend/src/main/components/Commons/FarmStats.js
+++ frontend/src/main/components/Commons/FarmStats.js
@@ -10,15 +10,15 @@ const FarmStats = ({userCommons}) => {
         <Card.Body>
             {/* update total wealth and cow health with data from fixture */}
             <Card.Text>
-                <img className="icon" src={Cash} alt="Cash"></img>
+                <img className="icon small" src={Cash} alt="Cash"></img>
             </Card.Text>
-            <Card.Text>
+            <Card.Text className="bigger-text">
                 Total Wealth: ${userCommons.totalWealth}
             </Card.Text>
             <Card.Text>
-                <img className="icon" src={Health} alt="Health"></img> 
+                <img className="icon small" src={Health} alt="Health"></img> 
             </Card.Text>
-            <Card.Text>
+            <Card.Text className="bigger-text">
                 Cow Health: {Math.round(userCommons.cowHealth*100)/100}%
             </Card.Text>
         </Card.Body>
