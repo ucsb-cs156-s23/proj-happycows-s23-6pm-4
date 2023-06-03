2c69e248e941648c68954e0ff5a32dd466d34b25
Jacob Woodward <jacobwoodward@ucsb.edu>
2023-06-03 00:23:01 -0700

    jw-add logic and tests for buying groups of cows in backend

--- src/test/java/edu/ucsb/cs156/happiercows/controllers/UserCommonsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/happiercows/controllers/UserCommonsControllerTests.java
@@ -174,8 +174,8 @@ public class UserCommonsControllerTests extends ControllerTestCase {
       .id(1L)
       .userId(1L)
       .commonsId(1L)
-      .totalWealth(300-testCommons.getCowPrice())
-      .numOfCows(2)
+      .totalWealth(280)
+      .numOfCows(3)
       .cowHealth(100)
       .build();
   
@@ -186,7 +186,7 @@ public class UserCommonsControllerTests extends ControllerTestCase {
       when(commonsRepository.findById(eq(1L))).thenReturn(Optional.of(testCommons));
   
       // act
-      MvcResult response = mockMvc.perform(put("/api/usercommons/buy?commonsId=1")
+      MvcResult response = mockMvc.perform(put("/api/usercommons/buy?commonsId=1&numCows=2")
           .contentType(MediaType.APPLICATION_JSON)
                       .characterEncoding("utf-8")
                       .content(requestBody)
@@ -252,7 +252,7 @@ public class UserCommonsControllerTests extends ControllerTestCase {
       when(commonsRepository.findById(eq(1L))).thenReturn(Optional.of(testCommons));
   
       // act
-      MvcResult response = mockMvc.perform(put("/api/usercommons/buy?commonsId=1")
+      MvcResult response = mockMvc.perform(put("/api/usercommons/buy?commonsId=1&numCows=1")
           .contentType(MediaType.APPLICATION_JSON)
                       .characterEncoding("utf-8")
                       .content(requestBody)
@@ -384,7 +384,7 @@ public class UserCommonsControllerTests extends ControllerTestCase {
       when(commonsRepository.findById(eq(234L))).thenReturn(Optional.of(testCommons));
   
       // act
-      MvcResult response = mockMvc.perform(put("/api/usercommons/buy?commonsId=234") 
+      MvcResult response = mockMvc.perform(put("/api/usercommons/buy?commonsId=234&numCows=1") 
           .contentType(MediaType.APPLICATION_JSON)
                       .characterEncoding("utf-8")
                       .content(requestBody)
@@ -589,7 +589,7 @@ public class UserCommonsControllerTests extends ControllerTestCase {
       when(commonsRepository.findById(eq(1L))).thenReturn(Optional.of(testCommons));
   
       // act
-      MvcResult response = mockMvc.perform(put("/api/usercommons/buy?commonsId=222")
+      MvcResult response = mockMvc.perform(put("/api/usercommons/buy?commonsId=222&numCows=1")
           .contentType(MediaType.APPLICATION_JSON)
                       .characterEncoding("utf-8")
                       .content(requestBody)
@@ -620,7 +620,7 @@ public class UserCommonsControllerTests extends ControllerTestCase {
       .id(1L)
       .userId(1L)
       .commonsId(1L)
-      .totalWealth(0)
+      .totalWealth(10)
       .numOfCows(1)
       .cowHealth(100)
       .build();
@@ -639,7 +639,7 @@ public class UserCommonsControllerTests extends ControllerTestCase {
       .id(1L)
       .userId(1L)
       .commonsId(1L)
-      .totalWealth(0)
+      .totalWealth(10)
       .numOfCows(1)
       .cowHealth(100)
       .build();
@@ -649,7 +649,7 @@ public class UserCommonsControllerTests extends ControllerTestCase {
       .id(1L)
       .userId(1L)
       .commonsId(1L)
-      .totalWealth(0)
+      .totalWealth(10)
       .numOfCows(1)
       .cowHealth(100)
       .build();
@@ -661,7 +661,74 @@ public class UserCommonsControllerTests extends ControllerTestCase {
       when(commonsRepository.findById(eq(1L))).thenReturn(Optional.of(testCommons));
   
       // act
-      MvcResult response = mockMvc.perform(put("/api/usercommons/buy?commonsId=1")
+      MvcResult response = mockMvc.perform(put("/api/usercommons/buy?commonsId=1&numCows=2")
+          .contentType(MediaType.APPLICATION_JSON)
+                      .characterEncoding("utf-8")
+                      .content(requestBody)
+                      .with(csrf()))
+              .andExpect(status().is(400)).andReturn();
+  
+      // assert
+      String responseString = response.getResponse().getContentAsString();
+      String expectedString = "{\"message\":\"You need more money!\",\"type\":\"NotEnoughMoneyException\"}";
+      Map<String, Object> expectedJson = mapper.readValue(expectedString, Map.class);
+      Map<String, Object> jsonResponse = responseToJson(response);
+      assertEquals(expectedJson, jsonResponse);
+  }
+
+  @WithMockUser(roles = { "USER" })
+  @Test
+  public void test_BuyCow_commons_exists_buy_zero_cows() throws Exception {
+  
+      // arrange
+  
+      UserCommons origUserCommons = UserCommons
+      .builder()
+      .id(1L)
+      .userId(1L)
+      .commonsId(1L)
+      .totalWealth(10)
+      .numOfCows(1)
+      .cowHealth(100)
+      .build();
+  
+      Commons testCommons = Commons
+      .builder()
+      .name("test commons")
+      .cowPrice(10)
+      .milkPrice(2)
+      .startingBalance(0)
+      .startingDate(LocalDateTime.now())
+      .build();
+  
+      UserCommons userCommonsToSend = UserCommons
+      .builder()
+      .id(1L)
+      .userId(1L)
+      .commonsId(1L)
+      .totalWealth(10)
+      .numOfCows(1)
+      .cowHealth(100)
+      .build();
+  
+      UserCommons correctuserCommons = UserCommons
+      .builder()
+      .id(1L)
+      .userId(1L)
+      .commonsId(1L)
+      .totalWealth(10)
+      .numOfCows(1)
+      .cowHealth(100)
+      .build();
+  
+      String requestBody = mapper.writeValueAsString(userCommonsToSend);
+      String expectedReturn = mapper.writeValueAsString(correctuserCommons);
+  
+      when(userCommonsRepository.findByCommonsIdAndUserId(eq(1L), eq(1L))).thenReturn(Optional.of(origUserCommons));
+      when(commonsRepository.findById(eq(1L))).thenReturn(Optional.of(testCommons));
+  
+      // act
+      MvcResult response = mockMvc.perform(put("/api/usercommons/buy?commonsId=1&numCows=0")
           .contentType(MediaType.APPLICATION_JSON)
                       .characterEncoding("utf-8")
                       .content(requestBody)
