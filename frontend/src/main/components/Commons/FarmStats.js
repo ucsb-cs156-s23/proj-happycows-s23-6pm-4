69f1940e5318e1df2c8df9eff8cea4c3a9cc803f
Krish Agarwal <kshitij0407@gmail.com>
2023-06-05 18:51:51 -0700

    Added a fix to show images on local host instead of alt text

---
+++ frontend/src/main/components/Commons/FarmStats.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import Health from "./../../../assets/Health.png";
+import Cash from "./../../../assets/Cash.png";
+
+const FarmStats = ({userCommons}) => {
+    return (
+        <Card className="text-center">
+        <Card.Header as="h5">Your Farm Stats</Card.Header>
+        <Card.Body>
+            {/* update total wealth and cow health with data from fixture */}
+            <Card.Text>
+                <img className="icon small" src={Cash} alt="Cash"></img>
+            </Card.Text>
+            <Card.Text className="bigger-text">
+                Total Wealth: ${userCommons.totalWealth}
+            </Card.Text>
+            <Card.Text>
+                <img className="icon small" src={Health} alt="Health"></img> 
+            </Card.Text>
+            <Card.Text className="bigger-text">
+                Cow Health: {Math.round(userCommons.cowHealth*100)/100}%
+            </Card.Text>
+        </Card.Body>
+        </Card>
+    ); 
+}; 
+
+export default FarmStats; 
